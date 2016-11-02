a71a1a9760b4a5dd4c529b64527f0765ce0e72a8
AndersTT <AndersTT@192.168.0.19>
2016-11-02 17:53:20 +0100

    switch operation 2

--- src/main/java/dk/dtu/ds/rmids/RmiDSPrintServant.java
+++ src/main/java/dk/dtu/ds/rmids/RmiDSPrintServant.java
@@ -38,7 +38,7 @@ public class RmiDSPrintServant extends UnicastRemoteObject implements RmiDSPrint
     @Override
     public String print(String filename, String printer) throws RemoteException
     {
-        return "Filename: xx" +"  -  " + "Printername: xx";
+        return "Filename: " + filename + "  -  " + "Printername: " + printer;
     }
     
     @Override
@@ -50,25 +50,25 @@ public class RmiDSPrintServant extends UnicastRemoteObject implements RmiDSPrint
     @Override
     public void topQueue(int job) throws RemoteException
     {
-        
+        System.out.println("topQueue");
     }
     
     @Override
     public void start() throws RemoteException
     {
-        
+        System.out.println("Start");
     }
     
     @Override
     public void stop() throws RemoteException
     {
-        
+        System.out.println("Stop");
     }
     
     @Override
     public void restart() throws RemoteException
     {
-        
+        System.out.println("Restart");
     }
     
     @Override
