4c8462df66670ad6e00e7a8e9e5d9b194c9bae54
steen <steen@DESKTOP-AQBDF90>
2016-11-02 13:45:28 +0100

    login

--- src/main/java/dk/dtu/ds/rmids/RmiDSPrintServant.java
+++ src/main/java/dk/dtu/ds/rmids/RmiDSPrintServant.java
@@ -1,8 +1,13 @@
 
 package dk.dtu.ds.rmids;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -14,6 +19,8 @@ public class RmiDSPrintServant extends UnicastRemoteObject implements RmiDSPrint
 {
     
     AESCrypto aes = new AESCrypto("ljksdf9342kjdfs9");
+    String username;
+    String password;
     
     public RmiDSPrintServant() throws RemoteException
     {
@@ -82,19 +89,39 @@ public class RmiDSPrintServant extends UnicastRemoteObject implements RmiDSPrint
 
     @Override
     public boolean login(User user) throws RemoteException {
-        
         try {
-            String username = aes.decrypt(user.getUsername());
-            String password = aes.decrypt(user.getPassword());
+            username = aes.decrypt(user.getUsername());
+            password = aes.decrypt(user.getPassword());
             
             
         } catch (Exception ex) {
             Logger.getLogger(RmiDSPrintServant.class.getName()).log(Level.SEVERE, null, ex);
         }
         
+        ArrayList<User> userList = new ArrayList<>();
+        String fileName = "user.bin";
         
+        try {
+          ObjectInputStream is = new ObjectInputStream(new FileInputStream(fileName));
+          userList = (ArrayList<User>) is.readObject();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(RmiDSPrintServant.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(RmiDSPrintServant.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(RmiDSPrintServant.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
-        
+        for (User users : userList) {
+            if(users.getUsername().equals(username))
+            {
+                String saltedPassword = password + users.getSalt();
+                if(users.getPassword().equals(saltedPassword.hashCode()))
+                {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 }
