e84ce4a2adfd890161dce6be0f596bb3e8cf5af7
AndersTT <AndersTT@192.168.0.19>
2016-11-02 13:47:12 +0100

    scanner client

--- src/main/java/dk/dtu/ds/rmids/RmiDSPrintClient.java
+++ src/main/java/dk/dtu/ds/rmids/RmiDSPrintClient.java
@@ -6,6 +6,8 @@ import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.Scanner;
+
 
 
 /**
@@ -14,10 +16,26 @@ import java.rmi.RemoteException;
  */
 public class RmiDSPrintClient 
 {
+    static String username;
+    static String password;
+    
     public static void main(String[] args) throws NotBoundException, MalformedURLException, RemoteException 
     {
         RmiDSPrintService print = (RmiDSPrintService) Naming.lookup("rmi://localhost:5019/printservice");
         System.out.println("***** " + print.ehco("Printer Ready ") + "*****");
- 
+        
+        TypeUsernameAndPassword();
     }
+
+    static void TypeUsernameAndPassword()
+    {
+        try (Scanner scanner = new Scanner(System.in)) 
+        {
+            System.out.println("Username:");
+            username = scanner.nextLine();
+            System.out.println("Password:");
+            password = scanner.nextLine();
+        }
+    }
+
 }
